1f60bfccd606203a259f7e8cd5136a01f7f5ca6f
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-06 20:08:44 +0500

    register all done

--- login.php
+++ login.php
@@ -1,73 +1,64 @@
 <?php require('top.php'); ?>
-	<!-- Start Banner Area -->
-	<section class="banner-area organic-breadcrumb">
-		<div class="container">
-			<div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
-				<div class="col-first">
-					<h1>Login/Register</h1>
-					<nav class="d-flex align-items-center">
-						<a href="index.html">Home<span class="lnr lnr-arrow-right"></span></a>
-						<a href="category.html">Login/Register</a>
-					</nav>
+<!--================Login Box Area =================-->
+<section class="login_box_area section_gap">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="login_form_inner">
+					<h3>Register</h3>
+					<form class="row login_form" id="register-form" method="post">
+						<div class="col-md-12 form-group">
+							<input type="text" class="form-control" name="name" id="name" placeholder="Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Name'">
+							<span class="field_error" id="name_error"></span>
+						</div>
+						<div class="col-md-12 form-group">
+							<input type="text" class="form-control" name="email" id="email" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'">
+							<span class="field_error" id="email_error"></span>
+						</div>
+						<div class="col-md-12 form-group">
+							<input type="text" class="form-control" name="mobile" id="mobile" placeholder="Mobile" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Mobile'">
+							<span class="field_error" id="mobile_error"></span>
+						</div>
+						<div class="col-md-12 form-group">
+							<input type="text" class="form-control" name="password" id="password" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+							<span class="field_error" id="password_error"></span>
+						</div>
+						<div class="col-md-12 form-group">
+							<button type="button" onclick="user_register()" class="primary-btn">Register</button>
+							<div class="form-output register_msg">
+								<p class="form-messege field_error"></p>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="login_form_inner">
+					<h3>Log in to enter</h3>
+					<form class="row login_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+						<div class="col-md-12 form-group">
+							<input type="text" class="form-control" id="name" name="name" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
+						</div>
+						<div class="col-md-12 form-group">
+							<input type="text" class="form-control" id="name" name="name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+						</div>
+						<div class="col-md-12 form-group">
+							<div class="creat_account">
+								<input type="checkbox" id="f-option2" name="selector">
+								<label for="f-option2">Keep me logged in</label>
+							</div>
+						</div>
+						<div class="col-md-12 form-group">
+							<button type="submit" value="submit" class="primary-btn">Log In</button>
+							<a href="#">Forgot Password?</a>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
-	</section>
-	<!-- End Banner Area -->
+	</div>
+</section>
 
-	<!--================Login Box Area =================-->
-	<section class="login_box_area section_gap">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="login_form_inner">
-						<h3>Register</h3>
-						<form class="row login_form" id="register-form" method="post">
-							<div class="col-md-12 form-group">
-								<input type="text" class="form-control" name="name" id="name" placeholder="Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Name'">
-							</div>
-							<div class="col-md-12 form-group">
-								<input type="text" class="form-control" name="email" id="email" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'">
-							</div>
-							<div class="col-md-12 form-group">
-								<input type="text" class="form-control" name="mobile" id="mobile" placeholder="Mobile" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Mobile'">
-							</div>
-							<div class="col-md-12 form-group">
-								<input type="text" class="form-control" name="password" id="password" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
-							</div>
-							<div class="col-md-12 form-group">
-								<button type="button" onclick="user_register()" class="primary-btn">Register</button>
-							</div>
-						</form>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<div class="login_form_inner">
-						<h3>Log in to enter</h3>
-						<form class="row login_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
-							<div class="col-md-12 form-group">
-								<input type="text" class="form-control" id="name" name="name" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
-							</div>
-							<div class="col-md-12 form-group">
-								<input type="text" class="form-control" id="name" name="name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
-							</div>
-							<div class="col-md-12 form-group">
-								<div class="creat_account">
-									<input type="checkbox" id="f-option2" name="selector">
-									<label for="f-option2">Keep me logged in</label>
-								</div>
-							</div>
-							<div class="col-md-12 form-group">
-								<button type="submit" value="submit" class="primary-btn">Log In</button>
-								<a href="#">Forgot Password?</a>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	
-	<!--================End Login Box Area =================-->
+<!--================End Login Box Area =================-->
 
 <?php require('footer.php'); ?>
