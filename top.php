3031c86a74a85c77b834a132aa45e64d5fbe2941
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-06 21:12:29 +0500

    at home

--- top.php
+++ top.php
@@ -93,7 +93,7 @@ if($mypage=='product.php'){
                         <ul class="nav navbar-nav menu_nav ml-auto">
                             <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
                             <li class="nav-item submenu dropdown">
-                                <a href="#" class="nav-link dropdown-toggle">My Profile</a>
+                                <a href="home.php" class="nav-link dropdown-toggle">My Profile</a>
                             </li>
                             <li class="nav-item submenu dropdown">
                                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
