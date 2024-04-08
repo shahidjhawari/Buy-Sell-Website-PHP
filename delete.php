ffe39398088dd641f17f11e6eafe83e645744511
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-08 18:21:57 +0500

    latest

--- delete.php
+++ delete.php
@@ -1,25 +1,20 @@
 <?php
-// Check if the ID parameter is provided in the URL
 if(isset($_GET['id']) && !empty($_GET['id'])) {
-    // Include database connection
     require('connection.inc.php');
 
-    // Prepare and execute SQL statement to delete the post
     $id = $_GET['id'];
     $sql = "DELETE FROM post WHERE id = ?";
     $stmt = $con->prepare($sql);
-    $stmt->bind_param('i', $id); // Assuming id is an integer
+    $stmt->bind_param('i', $id); 
     if($stmt->execute()) {
-        // Redirect back to the page where the post was deleted
-        header('Location: home.php'); // Replace previous_page.php with the actual page URL
+        header('Location: home.php'); 
         exit;
     } else {
         echo "Error deleting post: " . $con->error;
     }
 
 } else {
-    // If ID parameter is not provided or empty, redirect to an error page or homepage
-    header('Location: error_page.php'); // Replace error_page.php with the actual error page URL
+    header('Location: error_page.php'); 
     exit;
 }
 ?>
