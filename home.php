2769939763a15df60223c9944d2c81c648194fa5
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-10 20:08:48 +0500

    img as box done all

--- home.php
+++ home.php
@@ -17,58 +17,47 @@ $result = $stmt->get_result();
 ?>
 
 <style>
-    .own-btn {
+    .card-box {
         margin-top: 200px;
+        margin-bottom: 100px;
     }
 
-    @media screen and (min-width: 200px) and (max-width: 576px) {
-        .home-box {
-            margin-top: 120px;
-        }
+    .card {
+        margin-top: 20px;
     }
 </style>
 
-<div class="container home-box">
+<div class="container card-box">
     <div style='text-align: center;'><a href='post.php' class='btn btn-warning own-btn'><i class='fas fa-plus'></i></a>
         <p>Add new ad</p>
-        <?php if ($result->num_rows > 0) { ?>
-        <p>Please delete the post when it is sold out!</p>
     </div>
-        <div class="table-responsive">
-            <table class="table own-table">
-                <thead>
-                    <tr>
-                        <th scope="col">Image</th>
-                        <th scope="col">Ad Name</th>
-                        <th scope="col">Selling Price</th>
-                        <th scope="col">Edit</th>
-                        <th scope="col">Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php while ($row = $result->fetch_assoc()) : ?>
-                        <tr>
-                            <td><?php for ($i = 1; $i <= 1; $i++) {
-                                    $image_column = "image$i";
-                                    if (!empty($row[$image_column])) {
-                                        $image_path = PRODUCT_IMAGE_SITE_PATH . $row[$image_column];
-                                ?>
-                                        <img width="30px" src="<?php echo $image_path; ?>" alt="Image">
-                                <?php
-                                    }
-                                }
-                                ?></td>
-                            <td><?php echo $row['product_name']; ?></td>
-                            <td><?php echo "Rs. " . $row['price']; ?></td>
-                            <td><a href="edit.php?id=<?php echo $row['id']; ?>"><i class='fas fa-edit fa-1x' style="color: #191919;"></i></a></td>
-                            <td><a href="delete.php?id=<?php echo $row['id']; ?>"><i class='fas fa-trash fa-1x' style="color: #9A031E;"></i></a></td>
-                        </tr>
-                    <?php endwhile; ?>
-                </tbody>
-            </table>
+    <?php if ($result->num_rows > 0) { ?>
+        <div class="row">
+            <?php while ($row = $result->fetch_assoc()) : ?>
+                <div class="col-md-6 col-lg-3">
+                    <div class="card">
+                        <?php for ($i = 1; $i <= 1; $i++) {
+                            $image_column = "image$i";
+                            if (!empty($row[$image_column])) {
+                                $image_path = PRODUCT_IMAGE_SITE_PATH . $row[$image_column];
+                        ?>
+                                <img width="30px" class="card-img-top" src="<?php echo $image_path; ?>" alt="Image">
+                        <?php
+                            }
+                        }
+                        ?>
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $row['product_name']; ?></h5>
+                            <p class="card-text"><?php echo $row['phone_number']; ?></p>
+                            <p class="card-text"><?php echo "Rs. " . $row['price']; ?></p>
+                            <a href="edit.php?id=<?php echo $row['id']; ?>" class="btn btn-primary mr-2">Edit</a>
+                            <a href="delete.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
         </div>
     <?php } ?>
 </div>
-
 
 <?php require('footer.php'); ?>
