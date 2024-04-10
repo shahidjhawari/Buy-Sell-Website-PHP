6e7bbbc4b0816d626f5f59610c210d385ab59fb1
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-10 10:03:58 +0500

    latest

--- home.php
+++ home.php
@@ -37,26 +37,17 @@ $result = $stmt->get_result();
             <table class="table own-table">
                 <thead>
                     <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col">Product Name</th>
-                        <th scope="col">Product Detail</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Ad Name</th>
                         <th scope="col">Selling Price</th>
                         <th scope="col">Edit</th>
                         <th scope="col">Delete</th>
-                        <th>image</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php while ($row = $result->fetch_assoc()) : ?>
                         <tr>
-                            <td><?php echo $row['full_name']; ?></td>
-                            <td><?php echo $row['product_name']; ?></td>
-                            <td><?php echo $row['detail']; ?></td>
-                            <td><?php echo "Rs. " . $row['price']; ?></td>
-                            <td><a href="edit.php?id=<?php echo $row['id']; ?>"><i class='fas fa-edit fa-1x' style="color: #191919;"></i></a></td>
-                            <td><a href="delete.php?id=<?php echo $row['id']; ?>"><i class='fas fa-trash fa-1x' style="color: #9A031E;"></i></a></td>
-                            <td>
-                                <?php for ($i = 1; $i <= 1; $i++) {
+                            <td><?php for ($i = 1; $i <= 1; $i++) {
                                     $image_column = "image$i";
                                     if (!empty($row[$image_column])) {
                                         $image_path = PRODUCT_IMAGE_SITE_PATH . $row[$image_column];
@@ -65,8 +56,11 @@ $result = $stmt->get_result();
                                 <?php
                                     }
                                 }
-                                ?>
-                            </td>
+                                ?></td>
+                            <td><?php echo $row['product_name']; ?></td>
+                            <td><?php echo "Rs. " . $row['price']; ?></td>
+                            <td><a href="edit.php?id=<?php echo $row['id']; ?>"><i class='fas fa-edit fa-1x' style="color: #191919;"></i></a></td>
+                            <td><a href="delete.php?id=<?php echo $row['id']; ?>"><i class='fas fa-trash fa-1x' style="color: #9A031E;"></i></a></td>
                         </tr>
                     <?php endwhile; ?>
                 </tbody>
