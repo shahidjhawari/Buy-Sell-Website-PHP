161298c7770e10365e3da9d30ba5402d51fa5e38
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-10 19:22:39 +0500

    start post in box sec

---
+++ card.php
@@ -0,0 +1,66 @@
+<?php require('top.php'); ?>
+
+<style>
+    .card-box {
+      margin-top: 200px;
+      margin-bottom: 100px;
+    }
+    .card {
+        margin-top: 20px;
+    }
+  </style>
+
+<div class="container card-box">
+  <div class="row">
+    <div class="col-md-6 col-lg-3">
+      <div class="card">
+        <img src="https://via.placeholder.com/300" class="card-img-top" alt="Card Image">
+        <div class="card-body">
+          <h5 class="card-title">Card 1</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p class="card-text">$20</p>
+          <button class="btn btn-primary mr-2">Edit</button>
+          <button class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-3">
+      <div class="card">
+        <img src="https://via.placeholder.com/300" class="card-img-top" alt="Card Image">
+        <div class="card-body">
+          <h5 class="card-title">Card 2</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p class="card-text">$25</p>
+          <button class="btn btn-primary mr-2">Edit</button>
+          <button class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-3">
+      <div class="card">
+        <img src="https://via.placeholder.com/300" class="card-img-top" alt="Card Image">
+        <div class="card-body">
+          <h5 class="card-title">Card 3</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p class="card-text">$30</p>
+          <button class="btn btn-primary mr-2">Edit</button>
+          <button class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-3">
+      <div class="card">
+        <img src="https://via.placeholder.com/300" class="card-img-top" alt="Card Image">
+        <div class="card-body">
+          <h5 class="card-title">Card 4</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p class="card-text">$35</p>
+          <button class="btn btn-primary mr-2">Edit</button>
+          <button class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require('footer.php'); ?>
